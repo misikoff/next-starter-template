074d4aaa03587512525d63300347a7cf77ce250f
Thomas Misikoff <tmisikoff@gmail.com>
2022-01-01 18:02:22 -0600

    fix: icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
 
 import 'tailwindcss/tailwind.css'
 
@@ -7,6 +8,12 @@ import Layout from 'components/layout'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="h-full">
         <Head>
-          <meta name="application-name" content="Title" />
+          <meta name="application-name" content="" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="Title" />
+          <meta name="apple-mobile-web-app-title" content="" />
           <meta name="description" content="" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta
-            name="msapplication-config"
-            content="/icons/browserconfig.xml"
-          />
-          <meta name="msapplication-TileColor" content="#3a7ff3" />
-          <meta name="msapplication-tap-highlight" content="no" />
+
           <meta name="theme-color" content="#ffffff" />
 
           <link rel="apple-touch-icon" href="/icons-auto/apple-icon-180.png" />
@@ -56,65 +51,23 @@ class MyDocument extends Document {
           <link
             rel="mask-icon"
             href="/icons-auto/manifest-icon-192.maskable.png"
-            // color does not currently work
-            // color="#3a7ff3"
           />
           <link rel="icon" href="/favicon.ico" />
 
-          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:card" content="" />
           <meta name="twitter:url" content="" />
-          <meta name="twitter:title" content="Brand Title" />
-          <meta name="twitter:description" content="Cook your heart out!" />
+          <meta name="twitter:title" content="" />
+          <meta name="twitter:description" content="" />
           <meta name="twitter:image" content="" />
           <meta name="twitter:creator" content="" />
           <meta property="og:type" content="website" key="og:type" />
-          <meta property="og:title" content="Brand Title" key="og:title" />
-          <meta
-            property="og:description"
-            content="Cook your heart out!"
-            key="og:description"
-          />
-          <meta property="og:site_name" content="Brand Title" />
+          <meta property="og:title" content="" key="og:title" />
+          <meta property="og:description" content="" key="og:description" />
+          <meta property="og:site_name" content="" />
           <meta property="og:url" content="" key="og:url" />
           <meta property="og:image" content="" key="og:image" />
-
-          {/* apple splash screen images */}
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-2048-2732.jpg"
-            sizes="2048x2732"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-1668-2224.jpg"
-            sizes="1668x2224"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-1536-2048.jpg"
-            sizes="1536x2048"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-1125-2436.jpg"
-            sizes="1125x2436"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-1242-2208.jpg"
-            sizes="1242x2208"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-750-1334.jpg"
-            sizes="750x1334"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/icons-auto/apple-splash-640-1136.jpg"
-            sizes="640x1136"
-          />
         </Head>
+
         <body className="h-full">
           <Main />
           <NextScript />
